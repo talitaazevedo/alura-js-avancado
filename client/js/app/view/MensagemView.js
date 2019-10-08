4fdf885a390f64cceff6a7588ccda927877c215f
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-07 22:14:49 -0300

    adicionando herança nas classes => Implementando mensagens no projeto

---
+++ client/js/app/view/MensagemView.js
@@ -0,0 +1,10 @@
+//extends ou seja herda da classe view depende da classe view 
+class MensagemView extends View{
+    constructor(elemento){
+        super(elemento);
+    }
+    template(model){
+        return model.texto ? `<p class="alert alert-info>${model.texto}</p>` : '<p></p>';
+    }
+     
+}
