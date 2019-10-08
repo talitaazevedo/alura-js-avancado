60982bb9a7b632da1e0e2388c3219a6528b360b7
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-07 21:08:55 -0300

    finalizando o date helper

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -17,7 +17,8 @@ class DateHelper{
 
     static textoParaData(texto){
         //=> Expressão regular 
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('A data deve estar no formato aaaa-mm-dd');
+        if (!/^\d{4}-\d{2}-\d{2}/.test(texto))
+          throw new Error("A data deve estar no formato aaaa-mm-dd");
 
         // ... spread operator
         return new Date(...texto.split('-').map((item,indice)=> item - indice % 2));
